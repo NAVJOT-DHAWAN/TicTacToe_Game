ca510393dec6c0cfdec3e770c132945c032bc178
Navjot-Dhawan <navjotd139@gmail.com>
2020-05-22 20:10:33 +0530

    jumpTo method added to jump to particular move

--- src/index.js
+++ src/index.js
@@ -56,12 +56,13 @@ function Square(props) {
       history: [{                                              //Array to have record of all the previous moves
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();            //created shallow copy of existing array
     if (calculateWinner(squares) || squares[i]) {
@@ -73,21 +74,42 @@ function Square(props) {
       history: history.concat([{
         squares: squares
       }]),                                   
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2) === 0
+    })
   }
   
     render() {
 
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step,move) =>{
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      
+      return (
+        <li key={move}>
+          <button onClick = {() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;                                //identifying winner of the game
     } 
     else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');     //Identifying which player has next turn
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');     //Identifying which player has next turn
     }
 
       return (
@@ -100,7 +122,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
@@ -115,7 +137,7 @@ function Square(props) {
   );
   
 
-  
+
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
